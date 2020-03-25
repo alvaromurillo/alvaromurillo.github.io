5a717572099549a4238d7f16bcaa410228793968
Álvaro Murillo del Puerto <alvaromurillop@gmail.com>
2020-03-25 23:02:41 +0100

    75 card fixes

--- bingo/assets/js/script75.js
+++ bingo/assets/js/script75.js
@@ -6,24 +6,26 @@
 		fillCard(numbers);
 	}
 
-	function generateCard() {
-		var numbers = []
-		for (var i = 0; i < 25; i++) {
-			numbers.push(i+1);
-		}
-
+	function generateCard() {		
 		var cardNumbers = [];
-		for (var i = 0; i < 25; i++) {
-			var index = Math.floor(Math.random()*numbers.length);
-			var number = numbers[index];
-			cardNumbers.push(number);
-			numbers.splice(index, 1);
-		}
+		for (var i = 0; i < 5; i++) {
+			var numbers = []
+			for (var j = 0; j < 15; j++) {
+				numbers.push(i * 15 + j + 1);
+			}
+			for (var k = 0; k < 5; k++) {
+				var index = Math.floor(Math.random()*numbers.length);
+				var number = numbers[index];
+				cardNumbers.push(number);
+				numbers.splice(index, 1);
+			}
+		}		
+				
 		return cardNumbers;
 	}
 
 	function fillCard(numbers) {
-		var card = numbers.join("");
+		var card = numbers.map(i => ("0" + i).slice(-2)).join("");
 		for (var i = 0; i < 25; i++) {
 			if (i == 12) {
 				$('#cell'+i).addClass("qrCode");
