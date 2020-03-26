3f38f95935e47101c3f06ab8ae137e76a13e063f
Álvaro Murillo del Puerto <alvaromurillop@gmail.com>
2020-03-26 13:18:41 +0100

    Fix card 75

--- bingo/assets/js/script75.js
+++ bingo/assets/js/script75.js
@@ -9,32 +9,47 @@
 	function generateCard() {		
 		var cardNumbers = [];
 		for (var i = 0; i < 5; i++) {
-			var numbers = []
+			var columnNumbers = [];
+			var numbers = [];
 			for (var j = 0; j < 15; j++) {
 				numbers.push(i * 15 + j + 1);
 			}
 			for (var k = 0; k < 5; k++) {
 				var index = Math.floor(Math.random()*numbers.length);
 				var number = numbers[index];
-				cardNumbers.push(number);
+				columnNumbers.push(number);
 				numbers.splice(index, 1);
 			}
+			cardNumbers.push(columnNumbers)
 		}		
 				
 		return cardNumbers;
 	}
 
+	function generateQRText(numbers) {
+		var text = ""
+		for (var i = 0; i < 5; i++) {
+			for (var j = 0; j < 5; j++) {
+				text = text + ( "0" + numbers[j][i]).slice(-2);
+			}
+		}
+		return text
+	}
+
 	function fillCard(numbers) {
-		var card = numbers.map(i => ("0" + i).slice(-2)).join("");
-		for (var i = 0; i < 25; i++) {
-			if (i == 12) {
-				$('#cell'+i).addClass("qrCode");
-				jQuery('#cell'+i).qrcode({ text: card, correctLevel: 0 });
-				var canvas = $('canvas')[0];
-				$('#cell'+i).css("background-image", 'url(' + canvas.toDataURL() + ')');
-				canvas.remove();
-			} else {
-				$('#cell'+i).html(numbers[i]);
+		var card = generateQRText(numbers)
+		for (var i = 0; i < 5; i++) {
+			for (var j = 0; j < 5; j++) {
+				var index = i * 5 + j
+				if (index == 12) {
+					$('#cell'+index).addClass("qrCode");
+					jQuery('#cell'+index).qrcode({ text: card, correctLevel: 0 });
+					var canvas = $('canvas')[0];
+					$('#cell'+index).css("background-image", 'url(' + canvas.toDataURL() + ')');
+					canvas.remove();
+				} else {
+					$('#cell'+index).html(numbers[j][i]);
+				}
 			}
 		}
 	}
